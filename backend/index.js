3c3f0dd67bef9bc21d28ac4e4fd179466c88f6c6
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2025-01-20 21:40:21 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const userRouter = require('./src/routes/userRouter')
 const postRouter = require('./src/routes/postRouter')
+const reviewRouter = require('./src/routes/reviewRouter')
 const cors = require('cors');
 
 mongoose.connect('mongodb://root:rc43ogjdi9mdw3@detu.ddns.net:27017/webProject?authSource=admin');
@@ -14,6 +15,7 @@ app.use(express.static('public'));
 
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
+app.use('/review', reviewRouter);
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
