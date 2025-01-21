7ecde6d7094edcc44f2469168963a425652ffe19
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2025-01-21 23:01:32 +0100

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProfileView from "@/views/ProfileView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView,
+    },
     {
       path: '/',
       name: 'home',
