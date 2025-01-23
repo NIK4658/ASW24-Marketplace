3f99180e54f6ba6d1e32b012327a412b8ff36cde
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2025-01-23 19:21:48 +0100

    Create SubmitButton

---
+++ frontend/src/components/login/SubmitButton.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+  textField: {
+    type: String,
+    required: true
+  },
+  buttonType: {
+    type: String,
+    required: true
+  }
+});
+
+const emit = defineEmits(['submit']);
+
+function handleClick() {
+  emit('submit');
+}
+</script>
+
+<template>
+  <button :type="buttonType" @click="handleClick">{{ textField }}</button>
+</template>
+
+<style scoped>
+  button {
+		padding: 10px 15px;
+		background-color: #007bff;
+		color: white;
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+	}
+
+	button:hover {
+		background-color: #0056b3;
+	}
+</style>
