c4e274d4e8611de09253d0ce4d4bf1d60540dac4
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2025-01-24 20:23:53 +0100

    Add status to create user

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -38,7 +38,7 @@ exports.createUser = (req, res) => {
 
     user.save()
       .then((result) => {
-        res.json(result)
+        res.status(200).json(result)
       })
       .catch((error) => {
         res.status(500).json({message: 'Error creating the user', error})
