25af55fa2b36873b3154a05376643a3aef232ff9
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2025-01-31 16:27:13 +0100

    Create ProductPageView.vue

---
+++ frontend/src/views/ProductPageView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ProductPage from '@/components/ProductPage.vue'
+import FooterPage from "@/components/MainPageComponent/FooterPage.vue";
+import HeaderPage from "@/components/MainPageComponent/HeaderPage.vue";
+</script>
+
+<template>
+  <HeaderPage />
+  <ProductPage />
+  <FooterPage />
+</template>
