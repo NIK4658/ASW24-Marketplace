058257eddd683d84687ca23c912f3212976e6367
Pablo Vargas <pablo.vargasgrateron@studio.unibo.it>
2025-02-03 17:02:21 +0100

    Create ChatView

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -19,6 +19,11 @@ const router = createRouter({
       component: () => import('@/views/ProfileView.vue'),
     },
     {
+      path: '/chat',
+      name: 'chat',
+      component: () => import('@/views/ChatView.vue'),
+    },
+    {
       path: '/',
       name: 'home',
       component: () => import('@/views/HomeView.vue'),
