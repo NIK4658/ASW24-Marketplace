e59afd39842fb21fee64a447c7a7d08bad84c601
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2025-02-04 21:38:01 +0100

    Update chatModel.js

--- backend/src/models/chatModel.js
+++ backend/src/models/chatModel.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 const chatSchema = new mongoose.Schema({
   sender: {
-    type: mongoose.Schema.Types.ObjectId, required: true
+    type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true
   }, receiver: {
-    type: mongoose.Schema.Types.ObjectId, required: true
+    type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true
   }, message: {
     type: String, required: true
   }, read: {
