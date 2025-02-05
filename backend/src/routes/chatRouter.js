6d8a394336cc999153e5fc94a9bd4334fc400aa5
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2025-02-05 01:49:50 +0100

    Update chatController.js

--- backend/src/routes/chatRouter.js
+++ backend/src/routes/chatRouter.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const controller = require('../controllers/chatController');
 
 router.route('/')
-  .post(controller.createChat);
+  .post(controller.logChat);
 
 router.route('/:userId')
-  .get(controller.getChatsByUser);
+  .get(controller.getPreviewsByUser);
+
+router.route('/between/:userId1/:userId2')
+  .get(controller.getChatsBetweenUsers)
+  .post(controller.readChatsBetweenUsers)
+  .delete(controller.deleteChatsBetweenUsers);
 
 module.exports = router;
