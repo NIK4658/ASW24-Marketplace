460ac331b71c744492f44b18ec532b2e3abcfd5c
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2025-02-05 02:32:39 +0100

    Update ChatPage.vue

--- frontend/src/components/ChatPage.vue
+++ frontend/src/components/ChatPage.vue
@@ -1,10 +1,38 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import ChatList from '@/components/chat/ChatList.vue';
+import ChatWindow from '@/components/chat/ChatWindow.vue';
+
+const senderId = ref('')
+const receiverId = ref('')
+const chatPreviews = ref([])
+const chatMessages = ref([])
+
+const loadPreviews = async (id) => {
+  try {
+    const response = await axios.get(`/backend/chat/${id}`)
+    chatPreviews.value = response.data
+  } catch (error) {
+    console.error('Error fetching data: ', error)
+  }
+}
+
+onMounted(async () => {
+  const response = await axios.get('/backend/users/session', {
+    withCredentials: true,
+  })
+  senderId.value = response.data.userId
+  await loadPreviews(senderId.value)
+})
 
 </script>
 
 <template>
-  <div>
-
+  <div class="container">
+    <div class="layout">
+      <chat-list :chats="chatPreviews" />  
+    </div>
   </div>
 </template>
 
