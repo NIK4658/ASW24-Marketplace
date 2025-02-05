074bccc467fb411ca11f5b2184ddd3d64a62922d
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2025-02-05 02:49:23 +0100

    Create ChatWindow.vue

---
+++ frontend/src/components/chat/ChatWindow.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ChatInput from '@/components/chat/ChatInput.vue';
+</script>
+
+<template>
+  <div class="container">
+    <chat-input />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
