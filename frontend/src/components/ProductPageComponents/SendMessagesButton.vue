e442e95d3a6f6fe061604981b2a08bf3b17d56a3
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2025-04-08 15:26:12 +0200

    Create SendMessagesButton.vue

---
+++ frontend/src/components/ProductPageComponents/SendMessagesButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  product: Object,
+  userLogged: String,
+});
+</script>
+
+<template>
+  <button v-if="product?.seller?.username !== userLogged" class="secondary-btn">
+    Send a message to the seller
+  </button>
+</template>
+
+<style scoped>
+
+</style>
