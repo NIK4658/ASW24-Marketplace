9410409393d25411502f592481edfd64686eb3c0
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2025-04-09 17:24:18 +0200

    Update socketServer.js

--- backend/socketServer.js
+++ backend/socketServer.js
@@ -1,12 +1,18 @@
 const { Server } = require('socket.io');
 
+const userSocketMap = {}; // username => socket.id
+
 module.exports = (server, sessionMiddleware, corsRule) => {
   const io = new Server(server, { cors: corsRule });
   io.engine.use(sessionMiddleware);
 
   io.on('connection', (socket) => {
-    const session = socket.request.session;
-    console.log('User connected ');
+    const session = socket.request.session.user;
+
+    if (session !== undefined && session.username) {
+      userSocketMap[session.username] = socket.id;
+      console.log(`User ${session.username} connected with socket ${socket.id}`);
+    }
 
     socket.on("joinRoom", async ({ roomId }) => {
       socket.join(roomId);
@@ -17,8 +23,22 @@ module.exports = (server, sessionMiddleware, corsRule) => {
       io.to(roomId).emit("receiveMessage", chat);
     });
 
+    //Receive event from client
+    socket.on('buyNotificationServer', async ({ targetUser }) => {
+      const targetSocketId = userSocketMap[targetUser];
+      if (targetSocketId) {
+        io.to(targetSocketId).emit('buyNotificationClient');
+        console.log(`Utente ${targetUser} notificato correttamente`);
+      } else {
+        console.log(`Utente ${targetUser} non notificato`);
+      }
+    });
+
     socket.on('disconnect', () => {
-      console.log('User disconnected ' + session.user);
+      console.log('User disconnected ' + session.username);
+      if (session.username && userSocketMap[session.username] === socket.id) {
+        delete userSocketMap[session.username];
+      }
       session.destroy();
     });
   });
