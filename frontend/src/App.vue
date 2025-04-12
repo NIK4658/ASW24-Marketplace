8f7c5acd9825f3eabf6e75af9ee77b4afd02538d
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2025-04-12 18:20:04 +0200

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,36 +1,19 @@
 <script setup>
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
-import { ref, onMounted, onBeforeUnmount } from 'vue'
-import io from 'socket.io-client'
+import { onMounted, onBeforeUnmount } from 'vue'
 import HeaderPage from '@/components/MainPageComponent/HeaderPage.vue'
 import FooterPage from '@/components/MainPageComponent/FooterPage.vue'
 import NotificationComponent from './components/ProductPageComponents/NotificationComponent.vue'
+import { initialConnection, socket} from "@/socket.js";
+import { popupRef } from '@/composables/usePopupRef'
 
-const socket = ref(null)
-const popupRef = ref(null)
 const route = useRoute()
 const noHeaderFooterRoutesNames = ['signup', 'login']
 const showHeaderFooter = computed(() => !noHeaderFooterRoutesNames.includes(route.name))
 
-onMounted(() => {
-  socket.value = io('http://localhost:3000', { withCredentials: true })
-
-  socket.value.on('connect', () => {
-    console.log('✅ Socket connesso')
-  })
-
-  socket.value.on('buyNotificationClient', () => {
-    popupRef.value?.show('Hai ricevuto una notifica di acquisto!')
-  })
-
-  socket.value.on('newMessageNotificationClient', () => {
-    popupRef.value?.show('Hai un nuovo messaggio!')
-  })
-
-  socket.value.on('reviewNotificationClient', () => {
-    popupRef.value?.show('Hai ricevuto una recensione!')
-  })
+onMounted(async () => {
+  await initialConnection(popupRef)
 })
 
 onBeforeUnmount(() => {
