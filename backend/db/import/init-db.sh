0f4c5e9bc34d80faeaa01bcc8390424dccf545a1
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2025-05-12 18:18:45 +0200

    Fix docker container to import table

--- backend/db/import/init-db.sh
+++ backend/db/import/init-db.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-DB_NAME="dbMovies"
-DB_COLLECTION="movies"
+# This script loads each file and creates a mongodb collection named using the file name.
+# The Json files must be named 'db_<collection-name>.json' (example: 'db_users.json' become collection 'users')
+
+DB_NAME="webProject"
 
 echo "Importing databases"
 
-# shellcheck disable=SC2066
-for f in "docker-entrypoint-initdb.d/content/*.json"
+for file in /docker-entrypoint-initdb.d/content/*.json
 do
-    mongoimport --host localhost --db $DB_NAME --collection $DB_COLLECTION --file $f
+    collection=${file##*_}
+    collection=${collection%.*}
+    echo "Importing file $file in collection $collection"
+    mongoimport --host 127.0.0.1 --db "$DB_NAME" --collection "$collection" --file "$file"
 done
 
 echo "Imported data in $DB_NAME"
