0c79cdaf34e2398753514a139f89a3ec517f6247
Pablo Vargas <85257320+Zettaiki@users.noreply.github.com>
2025-05-17 00:24:33 +0200

    Update ChatView.vue

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -3,5 +3,15 @@ import ChatPage from '@/components/ChatPage.vue'
 </script>
 
 <template>
-  <ChatPage />
+  <div class="chatview-wrapper">
+    <ChatPage />
+  </div>
 </template>
+
+<style scoped>
+.chatview-wrapper {
+  padding-top: var(--header-height);
+  padding-bottom: var(--footer-height);
+  box-sizing: border-box;
+}
+</style>
