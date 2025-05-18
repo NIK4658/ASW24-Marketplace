14afc7142ddb6dfb5cadb8ea7568472fd2e13a67
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2025-05-18 22:28:17 +0200

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,11 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
