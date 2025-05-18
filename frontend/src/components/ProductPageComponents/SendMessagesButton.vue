734205bf967838ec397832aac037e9ef5021e928
Nicola Montanari <Nicola.Montanari14@studio.unibo.it>
2025-05-18 21:57:56 +0200

    Update SendMessagesButton.vue

--- frontend/src/components/ProductPageComponents/SendMessagesButton.vue
+++ frontend/src/components/ProductPageComponents/SendMessagesButton.vue
@@ -1,12 +1,23 @@
 <script setup>
-defineProps({
-  product: Object,
+import { useRouter } from "vue-router";
+
+const props = defineProps({
+  seller: String,
   userLogged: String,
 });
+const router = useRouter();
+const sendMessage = async () => {
+  try {
+    await router.push({ name: 'chat', params: { username: props.seller } });
+    window.location.reload();
+  } catch (error) {
+    console.error('Errore durante la richiesta di messaggio: ', error);
+  }
+};
 </script>
 
 <template>
-  <button v-if="product?.seller?.username !== userLogged" class="secondary-btn">
+  <button @click="sendMessage" v-if="props.seller !== userLogged" class="secondary-btn">
     Send a message to the seller
   </button>
 </template>
